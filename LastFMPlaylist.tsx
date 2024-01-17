7fdcc9479bfef409e3514bb8c181d8f90d815d22
Greg Pappas <gnpappas@proton.me>
2024-01-17 13:50:48 -1000

    init commit

---
+++ LastFMPlaylist.tsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import sound from "./sound.gif";
+
+interface LFMProps {
+  user: string,
+  refresh: number,
+  limit: number
+}
+
+interface SongTypes {
+  artist: { "#text": string },
+  name: string,
+  url: string,
+  image: { "#text": string }[]
+}
+
+
+const LastFMPlaylist = ({ user, refresh, limit }: LFMProps) => {
+  const LFM = import.meta.env.VITE_LASTFM;
+  const [songs, setSongs] = useState([]);
+  const [songError, setSongError] = useState("");
+  const PLACEHOLDER_ART: string = "2a96cbd8b46e442fc41c2b86b821562f.png";
+
+  // Minimal Styling
+  const SONGS_LIST: object = {
+    display: "flex",
+    flexDirection: "column",
+    gap: "4px",
+    listStyle: "none"
+  }
+  const CURRENTLY_PLAYING: object = {
+    display: "flex",
+    alignItems: "center",
+    gap: "8px",
+    fontWeight: "700",
+    borderRadius: "0.5em"
+  }
+  const ALBUM_ART: object = { borderRadius: "0.5em", width: "64px" }
+
+  const getSongs = async () => {
+    try {
+      const response = await axios.get(
+        `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${LFM}&limit=${limit}&format=json`
+      );
+      const data = await response.data.recenttracks.track;
+      setSongs(data);
+    } catch (error) {
+      setSongError("Error fetching songs... ;(")
+    }
+  }
+
+  useEffect(() => {
+    getSongs();
+    const interval = setInterval(getSongs, (refresh * 1000));
+    return () => clearInterval(interval);
+  }, [])
+
+  return (
+    <ul style={SONGS_LIST}>
+      {songs.length === 0 &&
+        <>
+          {songError.length === 0 ?
+            <li>Loading songs...</li>
+            :
+            <li>{songError}</li>}
+        </>
+      }
+      {songs.length === 10 &&
+        <li style={{ fontWeight: "700" }}>No music playing</li>
+      }
+      {songs.map(({ artist, name, url, image }: SongTypes, index) =>
+        <li key={index}>
+          <>
+            {songs.length === 11 && index === 0 ?
+              <>
+                {image[1]["#text"].slice(-36) === PLACEHOLDER_ART ?
+                  <div style={CURRENTLY_PLAYING}>
+                    <img src={sound} style={ALBUM_ART} />
+                    <a href={url} target="_blank">{artist["#text"]} - {name}</a>
+                  </div>
+                  :
+                  <div style={CURRENTLY_PLAYING}>
+                    <img src={image[3]["#text"]} style={ALBUM_ART} />
+                    <img src={sound} />
+                    <a href={url} target="_blank">{artist["#text"]} - {name}</a>
+                  </div>
+                }
+              </>
+              :
+              <a href={url} target="_blank">{artist["#text"]} - {name}</a>
+            }
+          </>
+        </li>
+      )}
+    </ul>
+  )
+}
+
+export default LastFMPlaylist
---
+++ README.md
@@ -0,0 +1,50 @@
+# lastfm_playlist
+
+## About
+
+Lastfm_playlist is designed to work with Vite/React combo. It allows you to display the current song playing with album art and a list of songs previously played. If there's no song playing, it at the top of the list "No music playing". If there's placeholder album art, I have removed the art with just the text showing instead since it's pretty ugly.
+
+## Example
+
+You can see it in action on my [webpage](https://vagabond.sh) or see the example below.
+
+## Screenshot
+
+This example is using custom styling which is not included.
+
+<img src="./example.png" width="500px" alt="styled example"/>
+
+## API Key
+
+You will need an last.fm API key for this to work. You can get one from [here](https://www.last.fm/api/account/create).
+You will then want to add the key to your .env file. If you don't have one, create one in your root directory and then add the following.
+
+```tsx
+VITE_LASTFM = "API_KEY_HERE";
+```
+
+## Import Component
+
+Put the following at the top of your component where you want to show your music.
+
+```tsx
+import LastFMPlayed from "lastfm_played";
+```
+
+## Component with props
+
+Finally, put the component in your code as desired.
+
+```tsx
+<LastFMPlayed user={"username"} refresh={30} limit={10} />
+```
+
+- **user**: _string_ - Put your username here.
+- **refresh**: _number_ - Put the number of seconds you want the component to refresh to keep the list updated.
+- **limit**: _number_ - Shows how many songs in your list. I believe this caps out at 200.
+
+## Styling
+
+I have only provided the minimal amount of styling to keep it open for personal styling preference. When imported in a project with no CSS, it'll look as such:
+
+<img src="./example_nostyle.png" width="500px" alt="unstyled example"/>
